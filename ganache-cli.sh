793a061cb6f532ca612b400fed6d447faf8bded1
Mihail Ozarinschi <mihail.ozarinschi@outlook.com>
2018-02-12 18:36:16 +0200

    chore: use ganache-cli instead of testrpc

--- ganache-cli.sh
+++ ganache-cli.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-# Executes cleanup function at script exit.
-trap cleanup EXIT
-
-cleanup() {
-  # Kill the ganache-cli instance that we started (if we started one).
-  if [ -n "$ganache_cli_pid" ]; then
-    kill -9 $ganache_cli_pid
-  fi
-}
-
-ganache_cli_running() {
-  nc -z localhost 8444
-}
-
-if ganache_cli_running; then
-  echo "Using existing ganache-cli instance"
-else
-  echo "Starting ganache-cli to run unit tests"
-  # We define 10 accounts with balance 1M ether, needed for high-value tests.
-  ./node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff --port 8444 \
+./node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff --port 8444 \
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200,1000000000000000000000000000" -u 0 \
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201,1000000000000000000000000000" -u 1 \
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202,1000000000000000000000000000" -u 2 \
@@ -29,9 +10,4 @@ else
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501206,1000000000000000000000000000" -u 6 \
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207,1000000000000000000000000000" -u 7 \
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208,1000000000000000000000000000" -u 8 \
-    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000000" -u 9 \
-  > /dev/null &
-  ganache_cli_pid=$!
-fi
-
-./node_modules/.bin/truffle test
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000000" -u 9
--- truffle.js
+++ truffle.js
@@ -3,13 +3,11 @@ module.exports = {
         development: {
             host: "localhost",
             port: 8444,
-            network_id: "*",
-            gas: 0xfffffffffff,
-            gasPrice: 0x01
+            network_id: "*"
         },
         coverage: {
             host: "localhost",
-            network_id: "675341",
+            network_id: "*",
             port: 8555,
             gas: 0xfffffffffff,
             gasPrice: 0x01
